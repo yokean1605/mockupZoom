1f1c011c9999b83fc05afa9d7e40013efd910470
yokean1605 <yoke.mahardika@gmail.com>
2020-07-23 20:04:26 +0700

    first-commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+(function ($) {
+  "use strict"; // Start of use strict
+  $('.your-class').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 2
+  });
+})(jQuery); // End of use strict
